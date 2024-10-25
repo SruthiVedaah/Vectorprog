a3b828555d71776e04e0c4771c29a0e50f21348b
Sruthi <psruthigopal1@gmail.com>
2024-10-25 11:17:27 +0530

    Initial Commit

---
+++ Vectorprog/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Vectorprog
+//
+//  Created by SRUTHI on 25/10/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
